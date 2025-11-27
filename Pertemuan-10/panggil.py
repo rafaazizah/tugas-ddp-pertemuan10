b14c051690fd5c673d490218907e0d02321f2557
rafaazizah <rafaazizah772@gmail.com>
2025-11-27 16:09:36 +0700

    Add files via upload

---
+++ Pertemuan-10/panggil.py
@@ -0,0 +1,15 @@
+import bangun_datar, bangun_ruang
+
+print("====Luas Bangun Datar====")
+print(f"luas persegi = {bangun_datar.luas_segitiga(5,5)}")
+print(f"luas segitiga = {bangun_datar.luas_segitiga(5,5)}")
+print(f"luas lingkaran = {bangun_datar.luas_segitiga(5,5)}")
+print(f"luas ketupat = {bangun_datar.luas_segitiga(5,5)}")
+print(f"luas jajar genjang = {bangun_datar.luas_segitiga(5,5)}")
+
+print("====Luas Bangun Ruang====")
+print(f"luas kubus = {bangun_datar.luas_segitiga(5,5)}")
+print(f"luas balok = {bangun_datar.luas_segitiga(5,5)}")
+print(f"luas bola = {bangun_datar.luas_segitiga(5,5)}")
+print(f"luas tabung = {bangun_datar.luas_segitiga(5,5)}")
+
